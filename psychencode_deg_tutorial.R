3aefcbb86920a6c9abb05a168553e573a56b4d24
stripathy <stripathy@node21.camhres.ca>
2023-06-20 17:07:38 -0400

    asked chatgpt to add some comments to code

--- psychencode_deg_tutorial.R
+++ psychencode_deg_tutorial.R
@@ -3,80 +3,102 @@ library(tidyverse)
 library(cowplot)
 library(ggrepel)
 
-# load in pseudobulk expression counts from PsychEncode
+# Load pseudobulk expression counts from PsychEncode
 pseudo_fine = read_tsv('/external/rprshnas01/netdata_kcni/stlab/SZBDMulticohort/pseudobulk_fine_grained.counts.tsv')
-pseudo_fine[1:5, 1:10] # these are counts!
+pseudo_fine[1:5, 1:10] # These are counts!
 
-# load in pseudobulk metadata from PsychEncode
+# Load pseudobulk metadata from PsychEncode
 pseudo_meta = read_tsv('/external/rprshnas01/netdata_kcni/stlab/SZBDMulticohort/pseudobulk_fine_grained.metadata.tsv')
 
-# create a new object, pseudo_fine_cpm, with the pseudobulk counts but normalized as counts per million
+# Create a new object, pseudo_fine_cpm, with the pseudobulk counts normalized as counts per million
 pseudo_fine_cpm = edgeR::cpm(y = t(pseudo_fine[, 3:17660]))
 
-pseudo_fine_cpm_trans = cbind(pseudo_fine[, 1:2,], t(pseudo_fine_cpm)) # this adds back on the metadata about cell types and individuals
+# Add the metadata about cell types and individuals back to the pseudobulk counts
+pseudo_fine_cpm_trans = cbind(pseudo_fine[, 1:2,], t(pseudo_fine_cpm))
 
-# merge pseudobulk metadata and cpm data frame
+# Merge pseudobulk metadata and cpm data frame
 pseudo_fine_cpm_trans = left_join(pseudo_meta, pseudo_fine_cpm_trans)
 
-# make a plot that shows gene expression of a gene (SST below) vs all cell types
-pseudo_fine_cpm_trans %>% ggplot(aes(x = Celltype, y = log2(SST+1), fill = Phenotype)) + geom_boxplot() + theme_cowplot() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+# Make a plot that shows gene expression of a gene (SST below) vs all cell types
+pseudo_fine_cpm_trans %>% 
+  ggplot(aes(x = Celltype, y = log2(SST+1), fill = Phenotype)) + 
+  geom_boxplot() + 
+  theme_cowplot() + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
 
 
-#### set up information that we'll use for our stats model in limma-voom
+#### Set up information that we'll use for our stats model in limma-voom
 
-# set things up so we use a pseudobulk matrix from just SST cells from McLean cohort
-
+# Set things up to use a pseudobulk matrix from just SST cells from the McLean cohort
 USE_CELL_TYPE = 'In-PV_Basket'
 USE_COHORT = 'McLean'
 
-# this figures out which subjects have information for disease, Sex, PMI, and Age
-use_subjects = pseudo_meta %>% filter(Celltype == USE_CELL_TYPE, Cohort == USE_COHORT, num_cells > 50) %>% pull(unique_donor_ID)
-  
+# Identify subjects with information for disease, sex, PMI, and age
+use_subjects = pseudo_meta %>% 
+  filter(Celltype == USE_CELL_TYPE, Cohort == USE_COHORT, num_cells > 50) %>% 
+  pull(unique_donor_ID)
 
-disease = factor(pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% pull(Phenotype), levels = c('CON', 'SZ'))
-sex = pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% pull(Gender)
-pmi = pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% pull(PMI)
-age = pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% pull(Age)
-cells_per_donor = pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% pull(num_cells)
+disease = factor(pseudo_meta %>% 
+                   filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+                   pull(Phenotype), levels = c('CON', 'SZ'))
+sex = pseudo_meta %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+  pull(Gender)
+pmi = pseudo_meta %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+  pull(PMI)
+age = pseudo_meta %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+  pull(Age)
+cells_per_donor = pseudo_meta %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+  pull(num_cells)
 
-# count up how many subjects for each disease group
-pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% group_by(Phenotype) %>% tally
+# Count the number of subjects for each disease group
+pseudo_meta %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+  group_by(Phenotype) %>% 
+  tally
 
-pseudobulk_sst_counts = pseudo_fine %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE)
+# Extract the pseudobulk counts for SST cells of the selected subjects
+pseudobulk_sst_counts = pseudo_fine %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE)
 
 pseudobulk_sst_counts = pseudobulk_sst_counts[, 3:17660] %>% t()
 rownames(pseudobulk_sst_counts) = colnames(pseudo_fine[, 3:17660])
-colnames(pseudobulk_sst_counts) = pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% pull(unique_donor_ID)
+colnames(pseudobulk_sst_counts) = pseudo_meta %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+  pull(unique_donor_ID)
 
-### set up DGEList object 
-
+### Set up DGEList object 
 dge0 = DGEList(pseudobulk_sst_counts, genes = row.names(pseudo_fine_cpm))
 
-min_samples_expressing_gene <-  pseudo_meta %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% nrow * 0.8 # the 0.8 here refers to fraction of total samples that needs to express the gene
-dge0 = dge0[rowSums(dge0$counts >= 1) >= min_samples_expressing_gene, ] # this step filters genes such that they need to be detected in at least 80% of samples
+min_samples_expressing_gene <- pseudo_meta %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>% 
+  nrow * 0.8 # The 0.8 here refers to the fraction of total samples that needs to express the gene
+dge0 = dge0[rowSums(dge0$counts >= 1) >= min_samples_expressing_gene, ] # Filter genes that are detected in at least 80% of samples
 
-# dge0 = DGEList(pb_counts, group = pb_metadata$Phenotype)
 dge0 = calcNormFactors(dge0, method = "TMM")
 
-
-### set up design based on the factors defined above
+### Set up design based on the factors defined above
 design = model.matrix(~ age + pmi + sex + log10(cells_per_donor) + disease) 
 
-# do voom
+# Perform voom transformation
 vm = voom(dge0, design, plot = TRUE)
 
-# do lmFit and eBayes (btw what are these steps actually doing??)
+# Perform lmFit and eBayes steps
 fit = lmFit(vm, design)
 fit = eBayes(fit)
 
-### take object out of fit and then plot sex (here) and disease terms (below)
+### Plot genes associated with sex
 deg_table_sex = topTable(fit, coef = "sexMale",  n = Inf, sort = "none", adjust.method = "BH")
-# deg_table_sex$gene_symbol = colnames(pseudo_fine[, 3:17660])
 
 deg_table_sex %>% arrange(adj.P.Val) %>% head()
 
-# plot volcano plot of genes associated with sex
-sex_volcano = deg_table_sex %>% ggplot(aes(x = logFC, y = -log10(P.Value), label = genes)) + geom_point() +
+# Plot volcano plot of genes associated with sex
+sex_volcano = deg_table_sex %>% 
+  ggplot(aes(x = logFC, y = -log10(P.Value), label = genes)) + 
+  geom_point() +
   geom_text_repel(data = subset(deg_table_sex, -log10(P.Value) > 10), 
                   aes(label = genes), 
                   vjust = 1.5) +
@@ -84,14 +106,15 @@ sex_volcano = deg_table_sex %>% ggplot(aes(x = logFC, y = -log10(P.Value), label
 
 sex_volcano
 
-# do same as above for disease terms
+### Plot genes associated with disease
 deg_table_disease = topTable(fit, coef = "diseaseSZ",  n = Inf, sort = "none", adjust.method = "BH")
-# deg_table_disease$gene_symbol = colnames(pseudo_fine[, 3:17660])
 
 deg_table_disease %>% arrange(adj.P.Val) %>% head
 
-# plot volcano plot of genes associated with disease
-disease_volcano = deg_table_disease %>% ggplot(aes(x = logFC, y = -log10(P.Value), label = genes)) + geom_point() +
+# Plot volcano plot of genes associated with disease
+disease_volcano = deg_table_disease %>% 
+  ggplot(aes(x = logFC, y = -log10(P.Value), label = genes)) + 
+  geom_point() +
   geom_text_repel(data = subset(deg_table_disease, adj.P.Val < 0.1), 
                   aes(label = genes), 
                   vjust = 1.5) + 
@@ -101,16 +124,19 @@ disease_volcano = deg_table_disease %>% ggplot(aes(x = logFC, y = -log10(P.Value
 
 disease_volcano
 
-# make a plot that shows gene expression of a couple DE genes below
-arhgap18_plot = pseudo_fine_cpm_trans %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>%
+# Make plots that show gene expression of a couple of DE genes
+arhgap18_plot = pseudo_fine_cpm_trans %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>%
   ggplot(aes(x = Phenotype, y = log2(ARHGAP18+1))) + 
-  geom_boxplot() + theme_cowplot()
+  geom_boxplot() + 
+  theme_cowplot()
 
 arhgap18_plot
 
-znf641_plot = pseudo_fine_cpm_trans %>% filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>%
+znf641_plot = pseudo_fine_cpm_trans %>% 
+  filter(unique_donor_ID %in% use_subjects, Celltype == USE_CELL_TYPE) %>%
   ggplot(aes(x = Phenotype, y = log2(ZNF641+1))) + 
-  geom_boxplot() + theme_cowplot()
+  geom_boxplot() + 
+  theme_cowplot()
 
 znf641_plot
-
